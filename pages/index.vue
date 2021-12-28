a82101233bef4f9ab299091bdf4ed0f0848d3fb7
pengchihui1 <pengchihui1@gmail.com>
2021-12-28 15:18:42 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,58 +3,59 @@
     <div class="pc-brand">
       <span class="pc-brand__text">{{ title }}</span>
     </div>
-    <pc-drawing-pad v-model="drawing" ref="pad" />
-    <pc-button
-      label="undo"
-      @click="undo()" />
-      <pc-button
-        label="clear"
-        @click="clear()" />
-    <div style="display: flex; flex-direction: row; transform: scale(0.5);">
+    <pc-drawing-pad v-model="drawing" ref="pad" style="border: 1px solid red" />
+    <pc-button label="undo" @click="undo()">測試</pc-button>
+    <pc-button label="clear" @click="clear()"> 清除</pc-button>
+    <div style="display: flex; flex-direction: row; transform: scale(0.5)">
       <pc-drawing-pad
         v-model="normalizedDrawing"
         :height="255"
         :width="255"
-        :read-only="true" />
+        :read-only="true"
+      />
       <pc-drawing-pad
         v-model="normalizedDrawing"
         :height="255"
         :width="255"
-        :read-only="true" />
+        :read-only="true"
+      />
       <pc-drawing-pad
         v-model="normalizedDrawing"
         :height="255"
         :width="255"
-        :read-only="true" />
+        :read-only="true"
+      />
       <pc-drawing-pad
         v-model="normalizedDrawing"
         :height="255"
         :width="255"
-        :read-only="true" />
+        :read-only="true"
+      />
       <pc-drawing-pad
         v-model="normalizedDrawing"
         :height="255"
         :width="255"
-        :read-only="true" />
+        :read-only="true"
+      />
     </div>
     <h1>guess: {{ word }}</h1>
-    <pc-button
-      icon="right"
-      label="Start"
-      @click="start()" />
+    <pc-button icon="right" label="Start" @click="start()" />
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import PcButton from '~/components/button.vue';
-import PcDrawingPad from '~/components/drawing-pad.vue';
-import axios from 'axios';
+import PcButton from "~/components/button.vue";
+import PcDrawingPad from "~/components/drawing-pad.vue";
+import axios from "axios";
 
-import { throttle } from 'lodash';
+import { throttle } from "lodash";
 
 const guess = throttle(async (pad, drawing) => {
-  const result = await axios.post('http://localhost:4000/api/guess', { drawing });
+  const result = await axios.post("http://localhost:4000/api/guess", {
+    drawing,
+  });
+
   pad.normalizedDrawing = result.data.normalizedDrawing;
   pad.word = result.data.word;
 }, 100);
@@ -67,25 +68,24 @@ export default {
 
   data() {
     return {
-      title: 'Pictionary',
+      title: "Pictionary",
       drawing: [],
       normalizedDrawing: [[], []],
-      word: '',
+      word: "",
     };
   },
 
   watch: {
     drawing(drawing) {
-      console.log(drawing)
       guess(this, drawing);
     },
   },
-  
+
   methods: {
     undo() {
       this.$refs.pad.undo();
     },
-    
+
     clear() {
       this.$refs.pad.clear();
     },
